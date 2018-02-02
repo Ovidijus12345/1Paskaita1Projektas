029e379c13ddf4a3600af1335866e5b44fdd4b8d
Ovidijus12345 <o.kutkaitis@gmail.com>
2018-02-02 19:33:55 +0200

    first commit

--- src/uzdavinys1.java
+++ src/uzdavinys1.java
@@ -4,20 +4,26 @@ public class uzdavinys1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Iveskite savo varda.");
+        Scanner sc = new Scanner(System.in);// objekto nuskaitymui ir consoles
+        uzdavinys1 secondClassExample = new uzdavinys1(); // klases objektas
+        String name = "";
+        do{
+            System.out.println("Iveskite savo varda.");
+            name = sc.nextLine(); // nuskaito visa eilute
+            System.out.println(" Jusu vardas yra: " + name);
 
-        Scanner sc = new Scanner(System.in); // objekto nuskaitymui ir consoles
-        String name = sc.nextLine(); // nuskaito visa eilute
-        System.out.println(" Jusu vardas yra: " + name);
-        uzdavinys1 secondClassExample = new uzdavinys1();
-        int countChar = secondClassExample.countChars(name);
-        if (name.length() % 2 == 0) {
-            System.out.println("Ivestas zodis " + name + " yra lyginis, ilgis" + name.length() + "rasta raidziu a: " + countChar);
-        } else {
-            System.out.println(" Ivestas zodis " + name + "yra nelygus " + name.length() + "rasta raidziu a: " + countChar);
-        }
+            int lenght = name.length();
+
+
+            int countChar = secondClassExample.countChars(name);
+            if (name.length() % 2 == 0) {
+                System.out.println("Ivestas zodis " + name + " yra lyginis, ilgis " + name.length() + " rasta raidziu a: " + countChar);
+            } else {
+                System.out.println("Ivestas zodis " + name + "yra nelygus " + name.length() + " rasta raidziu a: " + countChar);
+            }
+
+        }while (!name.equals("pabaiga"));
     }
-//test
 
     private int countChars(String name) {
         int numb = 0;
