41938f539745f6667955fd51db92b9c494b4458d
Ovidijus12345 <o.kutkaitis@gmail.com>
2018-02-07 21:09:53 +0200

    Uzdavinys 3 catch + INTirDOUBLE + Paskaitia1Uzdavinys1+ Onute+mapai+uzdavinys+UzdaviniaiMap

---
+++ src/UzdaviniaiMap/Uzdavinys1.java
@@ -0,0 +1,40 @@
+package UzdaviniaiMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Uzdavinys1 {
+    public static void main (String[]args){
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Andrius");
+        names.add("Ovidijus");
+        names.add("Petras");
+        names.add("Jonas");
+        names.add("Tadas");
+
+        for(String name : names){
+
+            if(name.length() < 6){
+                System.out.println("Atitinka reikalavimus vardas: " + name);
+            }
+        }
+        System.out.println(metodas1(names));
+        System.out.println(metodas2(names));
+
+        List<Uzdavinys3> persons = new ArrayList<>();
+        Uzdavinys3 person = new Uzdavinys3("Ovidijus ", "Kutkaitis ", 28, "Kaunas ");
+        persons.add(person);
+        persons.add(new Uzdavinys3("Tadas ","Tramp ", 22,"Vilnius "));
+        for(Uzdavinys3 p : persons){
+            System.out.println(p.getName()+ p.getSurname()+ p.getCity()+p.getAge());
+        }
+    }
+    private static String metodas1 (List<String>names){
+       return names.get(0);
+    }
+    private static String metodas2 (List<String>names){
+        return names.get(4);
+    }
+}
+
---
+++ src/UzdaviniaiMap/Uzdavinys3.java
@@ -0,0 +1,33 @@
+package UzdaviniaiMap;
+
+import java.util.List;
+
+public class Uzdavinys3 {
+    private String name;
+    private String surname;
+    private int age;
+    private String city;
+
+    public Uzdavinys3(String name, String surname, int age, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
